0f70c2e91331d2177c3844431fd023301908032e
First Second <v1adp1exu5@gmail.com>
2019-01-02 12:04:50 +0300

    --- + SeederIntegrationalTests

--- Code/Questionnaire.Data/BusinessContext/Seeder.cs
+++ Code/Questionnaire.Data/BusinessContext/Seeder.cs
@@ -10,22 +10,70 @@ namespace Questionnaire.Data.BusinessContext
 {
     public static class Seeder
     {
-        static Seeder ()
-        {
-            RegionSheetTable = ExcelImporter.ImportData( @"SeedFiles/Regions.xlsx" );
-            CitySheetTable = ExcelImporter.ImportData( @"SeedFiles/Cities.xlsx" );
-            FirmTypeSheetTable = ExcelImporter.ImportData( @"SeedFiles/FirmTypes.xlsx" );
-            FirmSheetTable = ExcelImporter.ImportData( @"SeedFiles/FirmTypes.xlsx" );
-        }
+        public static string FileNameRegions { get; set; } = @"SeedFiles/Regions.xlsx";
+        public static string FileNameCities { get; set; } = @"SeedFiles/Cities.xlsx";
+        public static string FileNameFirmTypes { get; set; } = @"SeedFiles/FirmTypes.xlsx";
+        public static string FileNameFirms { get; set; } = @"SeedFiles/Firms.xlsx";
 
         public static SheetTable RegionSheetTable { get; private set; }
         public static SheetTable CitySheetTable { get; private set; }
         public static SheetTable FirmTypeSheetTable { get; private set; }
         public static SheetTable FirmSheetTable { get; private set; }
 
-        public static IEnumerable< Firm > GetRegions () => ExcelImporter.GetEnumerable< Firm >( RegionSheetTable );
-        public static IEnumerable< City > GetCities () => ExcelImporter.GetEnumerable< City >( CitySheetTable );
-        public static IEnumerable< FirmType > GetFirmTypes () => ExcelImporter.GetEnumerable< FirmType >( FirmTypeSheetTable );
-        public static IEnumerable< Firm > GetFirms () => ExcelImporter.GetEnumerable< Firm >( FirmSheetTable );
+        public static IEnumerable< Region > GetRegions ()
+        {
+            try {
+                if ( RegionSheetTable == default( SheetTable ) ) {
+                    RegionSheetTable = ExcelImporter.ImportData( FileNameRegions );
+                }
+
+                return ExcelImporter.GetEnumerable< Region >( RegionSheetTable );
+            }
+            catch {
+                return new Region[0];
+            }
+        }
+
+        public static IEnumerable< City > GetCities ()
+        {
+            try {
+                if ( CitySheetTable == default( SheetTable ) ) {
+                    CitySheetTable = ExcelImporter.ImportData( FileNameCities );
+                }
+
+                return ExcelImporter.GetEnumerable< City >( CitySheetTable );
+            }
+            catch {
+                return new City[0];
+            }
+        }
+
+        public static IEnumerable< FirmType > GetFirmTypes ()
+        {
+            try {
+                if ( FirmTypeSheetTable == default( SheetTable ) ) {
+                    FirmTypeSheetTable = ExcelImporter.ImportData( FileNameFirmTypes );
+                }
+
+                return ExcelImporter.GetEnumerable< FirmType >( FirmTypeSheetTable );
+            }
+            catch {
+                return new FirmType[0];
+            }
+        }
+
+        public static IEnumerable< Firm > GetFirms ()
+        {
+            try {
+                if ( FirmSheetTable == default( SheetTable ) ) {
+                    FirmSheetTable = ExcelImporter.ImportData( FileNameFirms );
+                }
+
+                return ExcelImporter.GetEnumerable< Firm >( FirmSheetTable );
+            }
+            catch {
+                return new Firm[0];
+            }
+        }
     }
 }
--- Tests/Questionnaire.Data.Tests/SeederIntegrationalTests.cs
+++ Tests/Questionnaire.Data.Tests/SeederIntegrationalTests.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Helpers;
 using NUnit.Framework;
 using Questionnaire.Data.BusinessContext;
 
@@ -13,96 +14,117 @@ namespace Questionnaire.Data.Tests
     [ TestFixture ]
     public class SeederIntegrationalTests
     {
-        [ Test ]
+        [Test]
         public void GetRegions_FileExists_Returns14Regions ()
         {
-            var res = Seeder.GetRegions();
+            // Arrange:
+            var oldFileName = Seeder.FileNameRegions;
+            Seeder.FileNameRegions = Seeder.FileNameRegions.AppendAssemblyPath();
 
-            Assert.That( 14 == res.Count() );
+            // Action:
+            var res = Seeder.GetRegions().ToList();
+
+            // Assert:
+            Assert.That( 14 == res.Count, $"res.Count: { res.Count }" );
+
+            // Clean:
+            Seeder.FileNameRegions = oldFileName;
         }
 
-        [ Test ]
+        [Test]
         public void Getegions_FileDoesNotExists_ReturnEmptyRegionCollection ()
         {
             // Arrange:
-            var originFile = Seeder.FileWithRegionsName;
-            Seeder.FileWithRegionsName = "";
+            var originFile = Seeder.FileNameRegions;
+            Seeder.FileNameRegions = "";
 
             // Action:
             var res = Seeder.GetRegions();
 
             // Assert:
             Assert.That( !res.Any() );
-            FileWithRegionsName = originFile;
+            Seeder.FileNameRegions = originFile;
         }
 
-        [ Test ]
+        [Test]
         public void GetCities_FileExists_Returns67Cities ()
         {
-            var res = Seeder.GetCities();
+            var oldFileName = Seeder.FileNameCities;
+            Seeder.FileNameCities = Seeder.FileNameCities.AppendAssemblyPath();
 
-            Assert.That( 67 == res.Count() );
+            var res = Seeder.GetCities().ToList();
+
+            Assert.That( 67 == res.Count, $"res.Count: { res.Count }" );
+            Seeder.FileNameCities = oldFileName;
         }
 
-        [ Test ]
+        [Test]
         public void GetCities_FileDoesNotExists_ReturnEmptyCityCollection ()
         {
             // Arrange:
-            var originFile = Seeder.FileWithCitiesName;
-            Seeder.FileWithCitiesName = "";
+            var originFile = Seeder.FileNameCities;
+            Seeder.FileNameCities = "";
 
             // Action:
             var res = Seeder.GetCities();
 
             // Assert:
             Assert.That( !res.Any() );
-            FileWithCitiesName = originFile;
+            Seeder.FileNameCities = originFile;
         }
 
         [ Test ]
         public void GetFirmTypes_FileExists_Returns10FirmTypes ()
         {
-            var res = Seeder.GetFirmTypes();
+            var oldFileName = Seeder.FileNameFirmTypes;
+            Seeder.FileNameFirmTypes = Seeder.FileNameFirmTypes.AppendAssemblyPath();
 
-            Assert.That( 10 == res.Count() );
+            var res = Seeder.GetFirmTypes().ToList();
+
+            Assert.That( 10 == res.Count, $"res.Count: { res.Count }"   );
+            Seeder.FileNameFirmTypes = oldFileName;
         }
 
         [ Test ]
         public void GetFirmTypes_FileDoesNotExists_ReturnEmptyFirmTypeCollection ()
         {
             // Arrange:
-            var originFile = Seeder.FileWithFirmTypesName;
-            Seeder.FileWithFirmTypesName = "";
+            var originFile = Seeder.FileNameFirmTypes;
+            Seeder.FileNameFirmTypes = "";
 
             // Action:
-            var res = Seeder.GetCities();
+            var res = Seeder.GetFirmTypes();
 
             // Assert:
             Assert.That( !res.Any() );
-            FileWithFirmTypesName = originFile;
+            Seeder.FileNameFirmTypes = originFile;
         }
 
         [ Test ]
         public void GetFirms_FileExists_Returns243Firms ()
         {
+            var oldFileName = Seeder.FileNameFirms;
+            Seeder.FileNameFirms = Seeder.FileNameFirms.AppendAssemblyPath();
+
             var res = Seeder.GetFirms();
 
             Assert.That( 243 == res.Count() );
+            Seeder.FileNameFirms = oldFileName;
         }
 
         [ Test ]
         public void GetFirms_FileDoesNotExists_ReturnEmptyFirmCollection ()
         {
             // Arrange:
-            var originFile = Seeder.FileWithFirmsName;
-            Seeder.FileWithFirmsName = "";
+            var originFile = Seeder.FileNameFirms;
+            Seeder.FileNameFirms = "";
 
             // Action:
             var res = Seeder.GetFirms();
 
             // Assert:
             Assert.That( !res.Any() );
-            FileWithFirmsName = originFile;
+            Seeder.FileNameFirms = originFile;
         }
 
     }
