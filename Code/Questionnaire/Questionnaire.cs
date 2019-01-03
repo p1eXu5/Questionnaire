6b1a2ad9283fdffe235658641a8418253a284cf4
First Second <v1adp1exu5@gmail.com>
2019-01-03 15:55:08 +0300

    -- Questionnaire project

---
+++ Code/Questionnaire/Questionnaire.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Questionnaire.Data.BusinessContext;
+
+namespace Questionnaire
+{
+    public class Questionnaire
+    {
+        private readonly IQuestionnaireBusinessContext _context;
+
+        public Questionnaire ( IQuestionnaireBusinessContext context )
+        {
+            _context = context ?? throw new ArgumentNullException( nameof( context ), "context cannot be null." ); ;
+        }
+    }
+}
---
+++ Code/Questionnaire/ReportMaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Questionnaire.Data.Models;
+
+namespace Questionnaire
+{
+    public static class ReportMaker
+    {
+        public static void MakeReport ( string fileName, IEnumerable< EmployeeAnswer > answers, ReportOrientation orientation = ReportOrientation.Horizontal )
+        {
+
+        }
+    }
+}
