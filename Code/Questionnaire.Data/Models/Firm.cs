80ba21c44cbdb299495875eded662621936ad1d1
First Second <v1adp1exu5@gmail.com>
2019-01-04 14:15:52 +0300

    -- Firm::Id

--- Code/Questionnaire.Data/Models/Firm.cs
+++ Code/Questionnaire.Data/Models/Firm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Questionnaire.Data.Models
 {
     public class Firm
     {
+        [ DatabaseGenerated( DatabaseGeneratedOption.None ) ]
         public int Id { get; set; }
         public string Name { get; set; }
 
